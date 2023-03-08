0d7532e8603feb1eb2b8cdf10734e27b03079fa9
tbgolfer86 <tbj86@outlook.com>
2023-03-08 11:18:40 -0500

    added to PUT and DELETE routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,12 +49,43 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryUpdate = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryUpdate) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryDelete = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryDelete) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
